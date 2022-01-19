48d137c6b3492e37b3094bb74cde553bb4e205f2
Eoin Harkin <eharky@gmail.com>
2022-01-19 16:48:22 +0000

    updated routes to render paths

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,22 @@
 const router = require('express').Router()
+const dbData = require('../db/db.js')
+
+
+
+router.get('/notes', (req,res) =>{
+    
+    dbData
+    .getNotes()
+    .then((note) =>{
+        return res.json(note)
+        
+    })
+    .catch((err) => res.status(500).json(err))
+    
+});
+
+//post
+
+//delete
+
 module.exports = router
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
-module.exports = router
 const path = require('path')
 
 router.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+module.exports = router
 
