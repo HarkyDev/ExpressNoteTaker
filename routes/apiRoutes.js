7e356c3e29686faf5cbdddcbf808bc53ea0fb89d
Eoin Harkin <eharky@gmail.com>
2022-01-19 17:42:50 +0000

    updated routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,42 @@
-const router = require('express').Router()
-const dbData = require('../db/db.js')
+const router = require('express').Router();
+
+const store = require('../db/db');
+
+// requesting the existing notes
+
+router.get('/notes', (req, res) => {
+    store
+        .getNotes()
+        .then(notes => {
+            res.json(notes)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+// posting note function route 
+
+router.post('/notes', (req, res) => {
+    console.log(req.body)
+    store
+        .addNote(req.body)
+        .then(note => {
+            res.json(note)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
 
 
+// delete note function route
 
-router.get('/notes', (req,res) =>{
-    
-    dbData
-    .getNotes()
-    .then((note) =>{
-        return res.json(note)
-        
-    })
-    .catch((err) => res.status(500).json(err))
-    
-});
-
-//post
-
-//delete
+router.delete('/notes/:id', (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err))
+})
 
 module.exports = router
