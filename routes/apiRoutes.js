7a7df67af087cb4a5e210cc3990667bf5e0918be
Eoin Harkin <eharky@gmail.com>
2022-01-23 00:05:03 +0000

    synced github build with that of the heroku cli repo

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const store = require('../db/db');
+const Notes = require('../db/db');
 
 // requesting the existing notes
 
 router.get('/notes', (req, res) => {
-    store
+    Notes
         .getNotes()
         .then(notes => {
             res.json(notes)
@@ -19,7 +19,7 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     console.log(req.body)
-    store
+    Notes
         .addNote(req.body)
         .then(note => {
             res.json(note)
@@ -33,7 +33,7 @@ router.post('/notes', (req, res) => {
 // delete note function route
 
 router.delete('/notes/:id', (req, res) => {
-    store
+    Notes
         .removeNote(req.params.id)
         .then(() => res.json({ ok: true }))
         .catch(err => res.status(500).json(err))
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const express = require('express');;
-const apiRoute = require('./Routes/apiRoutes.js');
-const htmlRoute = require('./Routes/htmlRoutes.js');
+const express = require('express');
+const apiRoute = require('./routes/apiRoutes.js')
+const htmlRoute = require('./routes/htmlRoutes.js')
+// const NoteDb = require ("./db/db.js")
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
-app.use('/api',apiRoute);
-app.use('/', htmlRoute);
+app.use('/api',apiRoute)
+app.use('/', htmlRoute)
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
 
 
 
